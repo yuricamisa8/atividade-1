027e4095a47dcfafdd012ca0eb29316df510b285
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-04 14:54:25 -0300

    first commit

---
+++ atividade 01.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba dois números do usuário e exiba a soma desses números.
+
+n1 = int(input("digite um numero:"))
+n2 = int (input("digite um segundo numero:"))
+
+soma = (n1 + n2)
+
+print(soma)
